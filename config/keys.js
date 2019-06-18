8bcba8883bc2adb7db26b839f32e19d112ae5370
Maduka Chidimma <maduka.chidimma@gmail.com>
2019-06-18 07:42:32 +0100

    ready for deployment

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://Hephzy:Hephzy@cluster0-9g1hg.mongodb.net/test?retryWrites=true&w=majority',
-    googleClientID: '356467546783-8ijn9ic2gtafhkbivbqqp6i957vc57ot.apps.googleusercontent.com',
-    googleClientSecret: 'nuNBk8e0nGJG6OIDCvdX4n_I'
+if(process.env.NODE_ENV === 'production'){
+   module.exports = require('./keys_prod');  
+} else {
+    module.exports = require('./keys_dev');
 }
--- routes/stories.js
+++ routes/stories.js
@@ -11,6 +11,7 @@ router.get("/", (req, res) => {
     status: "public"
   })
     .populate("user")
+    .sort({date: 'desc'})
     .then(stories => {
       res.render("stories/index", {
         stories: stories
@@ -24,12 +25,50 @@ router.get("/show/:id", (req, res) => {
     _id: req.params.id
   })
     .populate("user")
+    .populate('comments.commentUser')
     .then(story => {
-      res.render("stories/show", {
-        story: story
-      });
+     if(story.status == 'public'){
+       res.render("stories/show", {
+         story: story
+       })
+     } else{
+       if(req.user){
+        if(req.user.id === story.user._id){
+          res.render("stories/show", {
+            story: story
+        })
+      } else {
+        res.redirect('/stories')
+      }
+       }else{
+         res.redirect('/stories')
+       }
+     }
     });
 });
+
+// List stories from a user
+router.get('/user/:userId', (req, res) => {
+Story.find({user: req.params.userId, status: 'public'})
+.populate('user')
+.then(stories => {
+  res.render('stories/index', {
+    stories: stories
+  });
+});
+}); 
+
+// Logged in users stories
+router.get('/my', ensureAuthenticated, (req, res) => {
+  Story.find({user: req.user.id})
+  .populate('user')
+  .then(stories => {
+    res.render('stories/index', {
+      stories: stories
+    });
+  });
+  }); 
+  
 
 // Add story Form
 router.get("/add", ensureAuthenticated, (req, res) => {
@@ -42,9 +81,13 @@ router.get("/edit/:id", ensureAuthenticated, (req, res) => {
     _id: req.params.id
   })
     .then(story => {
-      res.render("stories/edit", {
-        story: story
-      });
+      if(story.user != req.user.id){
+        res.redirect('/stories');
+      }else{
+        res.render("stories/edit", {
+          story: story
+        });
+      }
     });
 });
 
@@ -96,6 +139,33 @@ router.put("/:id", (req, res) => {
       res.redirect("/dashboard");
     });
   });  
+});
+
+// Delete Story
+router.delete('/:id', (req, res) => {
+Story.remove({_id: req.params.id})
+.then(() => {
+  res.redirect('/dashboard');
+});
+});
+
+// Add Comments
+router.post('/comment/:id', (req, res) => {
+  Story.findOne({_id: req.params.id})
+  .then(story => {
+    const newComment = {
+    commentBody: req.body.commentBody,
+    commentUser: req.user.id
+    }
+
+    // Add to comments array
+    story.comments.unshift(newComment);
+
+    story.save()
+    .then(story => {
+      res.redirect(`/stories/show/${story.id}`);
+    });
+  });
 });
 
 module.exports = router;
